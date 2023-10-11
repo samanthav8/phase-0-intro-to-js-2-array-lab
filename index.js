890cf41005566da5803989d5a64ba98101cdc958
Samantha Villafana <samanthavillafana8@gmail.com>
2023-10-11 16:30:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+ function appendCat(name){ 
+    const catsCopy = [...cats];
+    catsCopy.push(name);
+    return catsCopy;
+ }
+
+ function prependCat(name){ 
+    const catsCopy = [...cats];
+    catsCopy.unshift(name);
+    return catsCopy;
+ }
+
+ function removeLastCat(){
+    const catsCopy = [...cats];
+    catsCopy.pop();
+    return catsCopy;
+ }
+
+ function removeFirstCat(){
+    const catsCopy = [...cats];
+    catsCopy.shift();
+    return catsCopy;
+ }
